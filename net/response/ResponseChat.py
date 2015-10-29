18f1994bd3d77fee838cc58b2624595393631d23
meishu <theme97@gmail.com>
2015-10-28 18:36:28 -0700

    implement ResponseChat

--- net/response/ResponseChat.py
+++ net/response/ResponseChat.py
@@ -6,7 +6,27 @@ from net.response.ServerResponse import ServerResponse
 class ResponseChat(ServerResponse):
     def execute(self, data):
         try:
-            # TODO
+            if self.main.state == Constants.GAMESTATE_PLAYING:
+                type = data.getInt32()
+
+                if type == 0: # whisper
+                    sender_name = data.getString()
+                    recipient_name = data.getString()
+                    message = data.getString()
+
+                    self.main.game.chat.addLine('[' + sender_name + ' -> ' + recipient_name + ']: ' + message)
+
+                elif type == 1: # global
+                    name = data.getString()
+                    message = data.getString()
+
+                    self.main.game.chat.addLine('[' + name + ']: ' + message)
+
+                elif type == 2: # whisper fail
+                    self.main.game.chat.addLine('<<Server>> Whisper failed.')
+
+                else:
+                    self.log('[' + str(Constants.S_CHAT) + '] ResponseChat - Unknown type ' + str(type))
 
             self.log('Received [' + str(Constants.S_CHAT) + '] ResponseChat')
         except:
