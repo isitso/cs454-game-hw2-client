3f9a7db2b01df99e3a6bc4a1a34457051f144447
meishu <theme97@gmail.com>
2015-10-28 23:44:20 -0700

    fix error in ResponseDisconnect

--- net/response/ResponseDisconnect.py
+++ net/response/ResponseDisconnect.py
@@ -1,15 +1,14 @@
+import sys
 from traceback import print_exc
 
 from common.Constants import Constants
 from net.response.ServerResponse import ServerResponse
 
-from sys import exit
-
 class ResponseDisconnect(ServerResponse):
     def execute(self, data):
         try:
             self.log('Received [' + str(Constants.S_DISCONNECT) + '] ResponseDisconnect')
-            exit()
+            sys.exit()
         except:
             self.log('Bad [' + str(Constants.S_DISCONNECT) + '] ResponseDisconnect')
             print_exc()
